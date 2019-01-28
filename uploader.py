ba2bb22cdb56ee3309a1be2a92e90bd0ff7c75e3
el-tocino <email+github@aforkinthetoad.com>
2019-01-28 00:28:14 -0600

    maybe handle both files?

--- uploader.py
+++ uploader.py
@@ -9,8 +9,10 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/precise/upload', methods=['POST'])
 def home():
     wavfile = request.files['audio']
+    metafile = request.files['metadata']
     name = str(int(time.time())) + ".wav"
     wavfile.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
+    metafile.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
     return jsonify({"status":"ok"})
         
 app.run(debug=True,host='0.0.0.0')
