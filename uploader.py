97d999aa0c4a7ce7a1f4cc6dfbcedf4564ebbd0f
el-tocino <email+github@aforkinthetoad.com>
2019-01-28 01:16:16 -0600

    better upload handler
    no longer requires alterations to mic.py.

--- uploader.py
+++ uploader.py
@@ -1,19 +1,28 @@
+
 import os
 import time
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-UPLOAD_FOLDER = '/home/pi/precise/upload'
+UPLOAD_FOLDER = '/home/ryan/precise/upload'
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/precise/upload', methods=['POST'])
 def home():
-    wavfile = request.files['audio']
-    metafile = request.files['metadata']
-    name = str(int(time.time())) + ".wav"
-    mname = name + ".meta"
-    wavfile.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
-    metafile.save(os.path.join(app.config['UPLOAD_FOLDER'], mname))
+    uploads = request.files
+    for precisefile in uploads:
+        fn = uploads[precisefile].filename
+        if fn == 'audio':
+            name = (str(int(time.time()))) + ".wav"
+            uploads[precisefile].save(os.path.join(app.config['UPLOAD_FOLDER'], name))
+            print("Saved audio file")
+        if fn == 'metadata':
+            name =  (str(int(time.time()))) + ".meta"
+            uploads[precisefile].save(os.path.join(app.config['UPLOAD_FOLDER'], name))
+            print("Saved metadata file")
+
     return jsonify({"status":"ok"})
-        
+
+
 app.run(debug=True,host='0.0.0.0')
+
