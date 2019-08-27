b7429490f4ed69e3f179e9f10d37476487050090
el-tocino <email+github@aforkinthetoad.com>
2019-08-26 21:07:32 -0500

    aarrrrgggggggghhhh!

--- DStest.py
+++ DStest.py
@@ -6,8 +6,8 @@ import argparse
 import time
 import wave
 import numpy as np
-from scipy.io import wavfile
-from requests import post, put, exceptions
+#from scipy.io import wavfile
+from requests import post
 from pydub import AudioSegment
 import rnnoise
 
@@ -25,19 +25,36 @@ def normalize(sound, target_dBFS):
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
 
-def denoise(sound):
+def frame_generator(frame_duration_ms,
+                    audio,
+                    sample_rate):
+    """Generates audio frames from PCM audio data.
+    Takes the desired frame duration in milliseconds, the PCM data, and
+    the sample rate.
+    Yields Frames of the requested duration.
+    """
+    n = int(sample_rate * (frame_duration_ms / 1000.0) * 2)
+    offset = 0
+    timestamp = 0.0
+    duration = (float(n) / sample_rate) / 2.0
+    while offset + n < len(audio):
+        yield audio[offset:offset + n]
+        offset += n       
+
+def dnoise(sound):
     """
     denoise clip via rnnoise
     """
+    denoiser = rnnoise.RNNoise()
     TARGET_SR = 48000
     #audio, sample_rate = rnnoise.read_wave(filename)
     sound = sound.set_frame_rate(TARGET_SR)
     sound.export('dnntemp.wav', format='wav')
     blah = wave.open('dnntemp.wav', 'rb')
     blah = blah.readframes(blah.getnframes())
-    frames = rnnoise.frame_generator(10, blah, TARGET_SR)
+    frames = frame_generator(10, blah, TARGET_SR)
     frames = list(frames)
-    tups = [rnnoise.denoiser.process_frame(frame) for frame in frames]
+    tups = [denoiser.process_frame(frame) for frame in frames]
     denoised_frames = [tup[1] for tup in tups]
     np_audio = np.concatenate([np.frombuffer(frame,
                                              dtype=np.int16)
@@ -78,38 +95,41 @@ parser.add_argument("-L", "--lowpass", help="low pass frequency.")
 parser.add_argument("-U", "--url", help="Deepspeech Server URL.", required="True")
 parser.add_argument("-D", "--denoise", help="de-noise clip.", action="store_true")
 parser.add_argument("-N", "--normalize", help="normalize clip.", action="store_true")
+parser.add_argument("-O", "--order", default="hldn", help="order of filters.")
 parser.add_argument("--targetdb", default=-22.0, help="DB target for normalize. (optional)")
 args = parser.parse_args()
 
 afn = args.wavfile
 dsurl = args.url
 start_time = time.time()
-print (start_time)
+print(start_time)
 ds_audio = AudioSegment.from_wav(afn)
 ds_audio = ds_audio.set_channels(1)
 short_silence = AudioSegment.silent(duration=250, frame_rate=16000)
 ds_audio = short_silence + ds_audio + short_silence
-print ("padding added", elapsedtime())
+print("padding added", elapsedtime())
 
-if args.lowpass is not None:
-    ds_audio = lpass(ds_audio, args.lowpass)
-    print (elapsedtime())
+for filter in list(args.order):
+    print(filter)
+    if filter.lower() == 'n' and args.normalize == True:
+        ds_audio = normalize(ds_audio, args.targetdb)
+        print("normalizing", elapsedtime())
 
-if args.highpass is not None:
-    ds_audio = hpass(ds_audio, args.highpass)
-    print (elapsedtime())
+    if filter.lower() == 'l' and args.lowpass is not None:
+        ds_audio = lpass(ds_audio, args.lowpass)
+        print("lowpass", elapsedtime())
 
-if args.denoise == 'True':
-    ds_audio = denoise(ds_audio)
-    print (elapsedtime())
+    if filter.lower() == 'h' and args.highpass is not None:
+        ds_audio = hpass(ds_audio, args.highpass)
+        print("highpass", elapsedtime())
 
-if args.normalize == 'True':
-    ds_audio = normalize(ds_audio, args.targetdb)
-    print (elapsedtime())
+    if filter.lower() == 'd' and args.denoise == True:
+        ds_audio = dnoise(ds_audio)
+        print("de-noising", elapsedtime())
 
 ds_audio.export('filteredclip.wav', format='wav')
 
-print (transcribe(afn).text)
-print (elapsedtime())
-print (transcribe('filteredclip.wav').text)
-print (elapsedtime())
+print(transcribe(afn).text)
+print(elapsedtime())
+print(transcribe('filteredclip.wav').text)
+print(elapsedtime())
